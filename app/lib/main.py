1b583bec049ae24da9e8cb85716e71dff4bdea7b
calebmutisya <mutisyacalebmusau@gmail.com>
2023-12-11 12:12:15 +0300

    Testing

--- app/lib/main.py
+++ app/lib/main.py
@@ -1,5 +1,6 @@
 from config import *
 from models import Customer, Review, Restaurant
+from sqlalchemy.sql import func
 #RESTAURANT
 #returns a collection of all the reviews for the `Restaurant`
 def reviews(restaurant_id):
@@ -12,29 +13,38 @@ def customers(restaurant_id):
 def fanciest():
     return session.query(Restaurant).order_by(Restaurant.price.desc()).first()
 #should return an list of strings with all the reviews for this restaurant
-def all_reviews(self):
-    return [review.full_review() for review in self.reviews]
+def all_reviews(restaurant_id):
+    restaurant = session.query(Restaurant).get(restaurant_id)
+    return [full_review(review) for review in restaurant.reviews]
 
 #CUSTOMER
 #returns the full name of the customer, with the first name and the last name  concatenated, Western style.
-def full_name(self):
-    return f"{self.first_name} {self.last_name}"
+def full_name(customer_id):
+    customer = session.query(Customer).get(customer_id)
+    return f"{customer.first_name} {customer.last_name}"
 
 #returns the restaurant instance that has the highest star rating from this customer
-def favorite_restaurant(self):
-    return max([review.restaurant for review in self.reviews], key= lambda r: r.average_rating())
+def favorite_restaurant(customer_id):
+    return (
+        session.query(Restaurant)
+        .join(Review)
+        .filter(Review.customer_id == customer_id)
+        .group_by(Restaurant.id)
+        .order_by(func.avg(Review.star_rating).desc())
+        .first()
+    )
 
 #takes a `restaurant` (an instance of the `Restaurant` class) and a rating- creates a new review for the restaurant with the given `restaurant_id`
-def add_review(self,restaurant,rating):
-    new_review= Review(customer=self, restaurant=restaurant, star_rating=rating)
+def add_review(customer, restaurant, rating):
+    new_review = Review(customer=customer, restaurant=restaurant, star_rating=rating)
     session.add(new_review)
     session.commit()
     return new_review
 
 #takes a `restaurant` (an instance of the `Restaurant` class) and
 #- removes **all** their reviews for this restaurant
-def delete_reviews(self, restaurant):
-    reviews_to_delete= [review for review in self.reviews if review.restaurant == restaurant]
+def delete_reviews(customer, restaurant):
+    reviews_to_delete = [review for review in customer.reviews if review.restaurant == restaurant]
     for review in reviews_to_delete:
         session.delete(review)
     session.commit()
@@ -42,23 +52,23 @@ def delete_reviews(self, restaurant):
 
 
 #should return a collection of all the reviews that the `Customer` has left
-def customer_reviews(self):
-    return self.reviews
+def customer_reviews(customer):
+    return customer.reviews
 
 #should return a collection of all the restaurants that the `Customer` has
-def reviewed_restaurants(self):
-    return [review.restaurant for review in self.reviews]
+def reviewed_restaurants(customer):
+    return [review.restaurant for review in customer.reviews]
 
 
 #REVIEW
 #should return a string formatted as follows:
 # Review for {insert restaurant name} by {insert customer's full name}: {insert review star_rating} stars.
-def full_review(self):
-    return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
+def full_review(review):
+    return f"Review for {review.restaurant.name} by {review.customer.full_name()}: {review.star_rating} stars."
 #should return the `Customer` instance for this review
-def reviewer(self):
-    return self.customer
+def reviewer(review):
+    return review.customer
 
 #should return the `Restaurant` instance for this review
-def reviewed_restaurant(self):
-    return self.restaurant
+def reviewed_restaurant (review):
+    return review.restaurant
--- app/lib/test.py
+++ app/lib/test.py
@@ -23,11 +23,74 @@ print("***** the restaurant that has the highest price *****")
 fanciest_restaurant = fanciest()
 print(f"The restaurant with the highest price is {fanciest_restaurant.name}")
 
+print("***** A list with all the reviews for the Restaurant Pitts Callahan *****")
+restaurant_id = 2
+all_reviews_list = all_reviews(restaurant_id)
+for review in all_reviews_list:
+    print(review)
 
 
 
 #Customer
 print(">>>>>>>>>>>> CUSTOMER OPERATIONS <<<<<<<<<<<")
 
+# customer_id = 1
+# customer_name = full_name(customer_id)
+# print(f"***** Full name of customer with ID {customer_id} *****")
+# print(f"Customer Full Name: {customer_name}")
+
+# # Test favorite_restaurant function
+# customer_id = 1
+# favorite_restaurant_instance = favorite_restaurant(customer_id)
+# print(f"***** The restaurant instance that has the highest star rating from this customer *****")
+# print(f"Favorite Restaurant: {favorite_restaurant_instance.name}")
+
+
+# # Test add_review function
+# customer = session.query(Customer).first()
+# restaurant = session.query(Restaurant).first()
+# rating = 5
+# new_review = add_review(customer, restaurant, rating)
+# print("***** Add a new review *****")
+# print(f"New Review ID: {new_review.id}, Star Rating: {new_review.star_rating}")
+
+
+# # Test delete_reviews function
+# delete_reviews(customer, restaurant)
+# print("***** Delete all reviews for the customer and restaurant *****")
+# print("Reviews after deletion:")
+# for review in customer_reviews(customer):
+#     print(f"Review ID: {review.id}, Star Rating: {review.star_rating}")
+
+
+# # Test customer_reviews function
+# print("***** A collection of all the reviews that the Customer has left *****")
+# customer_reviews_list = customer_reviews(customer)
+# for review in customer_reviews_list:
+#     print(f"Review ID: {review.id}, Star Rating: {review.star_rating}")
+
+
+# # Test reviewed_restaurants function
+# print("***** A collection of all the restaurants that the Customer has reviewed *****")
+# reviewed_restaurants_list = reviewed_restaurants(customer)
+# for reviewed_restaurant in reviewed_restaurants_list:
+#     print(f"Restaurant ID: {reviewed_restaurant.id}, Name: {reviewed_restaurant.name}")
+
+
 #Review
 print(">>>>>>>>>>>> REVIEW OPERATIONS <<<<<<<<<<<")
+
+# # Test full_review function
+# review = session.query(Review).first()
+# print("***** Full review details *****")
+# print(full_review(review))
+
+# # Test reviewer function
+# print("***** The Customer instance for this review *****")
+# reviewer_instance = reviewer(review)
+# print(f"Reviewer: {reviewer_instance.full_name()}")
+
+# # Test reviewed_restaurant function
+# print("***** The Restaurant instance for this review *****")
+# reviewed_restaurant_instance = reviewed_restaurant(review)
+# print(f"Reviewed Restaurant: {reviewed_restaurant_instance.name}")
