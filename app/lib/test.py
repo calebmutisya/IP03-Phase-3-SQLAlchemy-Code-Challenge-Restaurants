e735347e4937d440f6ffaa22118ff5f1124e30c0
calebmutisya <mutisyacalebmusau@gmail.com>
2023-12-11 09:29:59 +0300

    Test.py initialized

---
+++ app/lib/test.py
@@ -0,0 +1,10 @@
+from main import *
+
+#Restaurant
+print(">>>>>>>>>>>> RESTAURANT OPERATIONS <<<<<<<<<<<")
+
+#Customer
+print(">>>>>>>>>>>> CUSTOMER OPERATIONS <<<<<<<<<<<")
+
+#Review
+print(">>>>>>>>>>>> REVIEW OPERATIONS <<<<<<<<<<<")
